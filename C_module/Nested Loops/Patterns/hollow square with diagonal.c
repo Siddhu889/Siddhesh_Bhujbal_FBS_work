fffbcce93ab96b080886e2d14a27c7fec5cc9bcf
Siddhu889 <sbhujbal576@gmail.com>
2025-08-06 19:42:37 +0530

    Add files via upload

---
+++ C_module/Nested Loops/Patterns/hollow square with diagonal.c
@@ -0,0 +1,23 @@
+//Print a hollow square with diagonal pattern
+//Input: n = 5
+//Output:
+//
+//* * * * *
+//* *   * *
+//*   *   *
+//* *   * *
+//* * * * *
+#include <stdio.h>
+void main() {
+    int n = 5;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
+            if(i == 1 || i == n || j == 1 || j == n || i == j || j == (n - i + 1)) {
+                printf("* ");
+            } else {
+                printf("  ");
+            }
+        }
+        printf("\n");
+    }
+}
